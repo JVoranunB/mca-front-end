954b43843985b097c4c4e7bac9bb7701ff36afeb
JVoranunB <voranun.datasource@gmail.com>
2025-09-04 13:45:42 +0700

    chore: add the status line lib

---
+++ workflow-builder/src/utils/eventTypes.ts
@@ -0,0 +1,6 @@
+// CRM data source fields based on PostgreSQL schema
+export const EVENT_TYPES = {
+  user_created: "user_created",
+  user_info_updated: "user_info_updated",
+  user_point_info_updated: "user_point_info_updated",
+};
